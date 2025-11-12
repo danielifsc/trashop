1f92ec27b8396af540df0d83443c9f2cc402092e
dan <daniel.ma09@aluno.ifsc.edu.br>
2025-11-12 17:49:11 -0300

    linkando o bootstrap local

--- src/includes/rodape.php
+++ src/includes/rodape.php
@@ -21,7 +21,7 @@
     </footer>
   </section>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+  <script src="/src/assets/js/bootstrap.bundle.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
     crossorigin="anonymous"></script>
 </body>
