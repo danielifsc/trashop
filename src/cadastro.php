1c9dc1bb5ad4b197b0e68e01a992098998a65ccf
lucas kretschmar <lucas15kretschmar@gmail.com>
2025-11-12 18:20:22 -0300

    sfdafafas

--- src/cadastro.php
+++ src/cadastro.php
@@ -4,3 +4,27 @@ require_once 'src/includes/cabecalho.php' ;
 require_once 'src/includes/rodape.php';
 require_once 'src/includes/cabecalho.php';
 ?>
+
+
+
+
+
+
+
+<h1>asdfasfsfa</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+require_once 'src/includes/rodape.php';
+?>
