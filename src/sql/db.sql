f6e3e79d3955e947c8852a2bc3afd9b85f428474
dan <daniel.ma09@aluno.ifsc.edu.br>
2025-12-01 22:48:25 -0300

    mini update db

--- src/sql/db.sql
+++ src/sql/db.sql
@@ -13,18 +13,3 @@ CREATE TABLE IF NOT EXISTS `db_trashop`.`usuario` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci ;
-
--- Tabela de Atividades
--- CREATE TABLE `atividades` (
- -- `id` INT AUTO_INCREMENT PRIMARY KEY,
- -- `usuario_id` INT NOT NULL,
- -- `titulo` VARCHAR(255) NOT NULL,
- -- `descricao` TEXT,
- -- `status` ENUM('pendente', 'concluida') NOT NULL DEFAULT 'pendente',
- -- `data_criacao` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---  FOREIGN KEY (`usuario_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE
--- ) ENGINE=InnoDB DEFAULT
-
--- Tabela de carteira
-
-
